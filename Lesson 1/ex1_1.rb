d59f139efb30fe51611aa07bb492484c7eb5a653
Ravil <ravil.zaripov@gmail.com>
2021-03-30 19:10:42 +0300

    bugs fixed

--- Lesson 1/ex1_1.rb
+++ Lesson 1/ex1_1.rb
@@ -1,8 +1,8 @@
-# Урок 1 упражение 1 !
-puts "Как Вас зовут?"
+# Урок 1 упражение 1 
+puts 'Как Вас зовут?'
 name = gets.chomp
 
-puts "Какой у Вас рост?"
-rost = gets.chomp
+puts 'Какой у Вас рост?'
+height = gets.chomp.to_i
 
-puts "#{name}, привет! Твой идеальный вес #{ (( rost.to_i - 110 ) * 1.15).round(2)}"
+puts "#{name}, привет! Твой идеальный вес #{ (( height - 110 ) * 1.15).round(2)}"  
--- Lesson 1/ex1_3.rb
+++ Lesson 1/ex1_3.rb
@@ -1,40 +1,26 @@
-# Урок 1 упражение 3 !
+# Урок 1 упражение 3 
 
-puts "Укажите длину первой стороны треугольника:"
+puts 'Укажите длину первой стороны треугольника:'
 side1 = gets.chomp.to_f
 
-puts "Укажите длину второй стороны треугольника:"
+puts 'Укажите длину второй стороны треугольника:'
 side2 = gets.chomp.to_f
 
-puts "Укажите длину третьей стороны треугольника:"
+puts 'Укажите длину третьей стороны треугольника:'
 side3 = gets.chomp.to_f
 
+side1, side2, side3 = [side1,side2,side3].sort 
 puts "#{side1}, #{side2}, #{side3}"
 
 if side1 == side2 && side2 == side3
-	puts "Ваш треугольник равносторонний."
+	puts 'Ваш треугольник равносторонний.'
+elsif side1 == side2 || side2 == side3 || side1 == side3
+	puts 'Ваш треугольник равнобедренный.'
+else 
+	if 	side3 == (side2**2 + side1**2)**0.5
+		puts 'Ваш треугольник прямоугольный.'
+	else
+		puts 'Ваш треугольник - не равносторонний, не равнобедренный, не прямоугольный.'
+	end
 end
 
-if 	side1 == side2 && !(side1 == side3)
-	puts "Ваш треугольник равнобедренный."
-end
-
-if 	side1 == side3 && !(side1 == side2) 
-	puts "Ваш треугольник равнобедренный."
-end
-
-if 	side2 == side3 && !(side2 == side1)
-	puts "Ваш треугольник равнобедренный."
-end
-
-if 	side1 > side2 && side1 > side3 && side1 == (side2**2 + side3**2)**0.5
-	puts "Ваш треугольник прямоугольный."
-end
-
-if 	side2 > side1 && side2 > side3 && side2 == (side1**2 + side3**2)**0.5
-	puts "Ваш треугольник прямоугольный."
-end
-
-if 	side3 > side1 && side3 > side2 && side3 == (side1**2 + side2**2)**0.5
-	puts "Ваш треугольник прямоугольный."
-end
